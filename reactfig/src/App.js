c287634de0ed4d48f8c052fca968ede7b3cdd1dc
dodytrifa <dody.trifa@gmail.com>
2021-06-01 21:09:36 +0700

    add react sandbox

---
+++ reactfig/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="navbar">
+
+      </div>
+      <div className="social"></div>
+    </div>
+  );
+}
+
+export default App;
