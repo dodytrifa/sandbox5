44c88b466c794586c8b84fe38b8884d65400fa94
dodytrifa <dody.trifa@gmail.com>
2021-06-07 19:29:08 +0700

    exercise on blurry loading

--- reactfig/src/App.js
+++ reactfig/src/App.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Home from './pages/Home';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-
-
 function App() {
   return (
     <Router>
--- reactfig/src/components/Navbar.js
+++ reactfig/src/components/Navbar.js
@@ -1,45 +1,62 @@
-import React from 'react'
+import React from 'react';
 import styled from 'styled-components';
-
+import {NavLink as Link} from 'react-router-dom';
+import {FaBars} from 'react-icons/fa';
 
 const Navbar = () => {
   return (
-    <Wrapper>
-      <h4>Halo</h4>
-      <button>Ini Button lho bro</button>
-    </Wrapper>
+    <>
+      <Nav>
+        <NavLink>
+        <h4>Logo</h4>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about">Tentang Kami
+          </NavLink>
+          <NavLink to="/outlet">Semua Outlet
+          </NavLink>
+          <NavLink to="/promo">Promo
+          </NavLink>
+          <NavLink to="/karier">Karier
+          </NavLink>
+          <NavLink to="/download">Download App
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </>
   )
 }
-  const Wrapper = styled.nav`
-  padding: 1.5rem;
-  margin-bottom: 4rem;
-  background: var(--clr-white);
-  text-align: center;
-  display: grid;
-  grid-template-columns: auto auto 100px;
-  justify-content: center;
-  align-items: center;
-  gap: 1.5rem;
-  h4 {
-    margin-bottom: 0;
-    font-weight: 400;
-  }
-  img {
-    width: 35px !important;
-    height: 35px;
-    border-radius: 50%;
-    object-fit: cover;
-  }
-  button {
-    background: transparent;
-    border: transparent;
-    font-size: 1.2rem;
-    text-transform: capitalize;
-    letter-spacing: var(--spacing);
-    color: var(--clr-grey-5);
-    cursor: pointer;
-  }
-`;
+  
+export const Nav = styled.nav`
+  background: #000;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: .5rem calc(100vw - 1000px);
+  z-index: 5;
+`
 
+export const NavLink = styled(Link)`
+  background: #fff;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  height: 100%;
+`
+
+export const Bars = styled(FaBars)`
+  display: none;
+  color: #fff;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translated(-100%, 75%);
+    font-size: 1.8rem;
+  }
+`
 
 export default Navbar;
