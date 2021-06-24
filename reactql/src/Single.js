23321cc969bcd5503ee39d8b637175568ed4eca7
dodytrifa <dody.trifa@gmail.com>
2021-06-24 14:50:54 +0700

    setup files

--- reactql/src/Single.js
+++ reactql/src/Single.js
@@ -1,25 +1,36 @@
 import React, {useEffect,useState} from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 
 
 export default function Single() {
   const [posts, setPosts] = useState([])
 
-  useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts")
-    .then(res => {
-      console.log(res)
-      setPosts(res.data);
-    })
-    .catch(err => {
-      console.log(err);
-    })
+  // useEffect(() => {
+  //   axios.get("https://jsonplaceholder.typicode.com/posts")
+  //   .then(res => {
+  //     console.log(res)
+  //     setPosts(res.data);
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   })
     
-  },[])
+  // },[])
+
+  const getData = () => {
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    fetch("https://randomuser.me/api")
+    // fetch("https://prodev-api.ilcs.co.id/ibis_api_external_dev_v2/index.php/SingleBilling/getVessel?keyword=C&port=IDPNJ-PNJD")
+    .then((response) => response.json())
+    .then((json)=> {
+      console.log(json)
+      setPosts(json)
+    })
+  }
 
   return (
     <div>
-      <ul>
+      {/* <ul>
           {
             posts.map(post => {
              const {id, title} = post
@@ -28,7 +39,12 @@ export default function Single() {
              )
             })
           }
-        </ul>
+        </ul> */}
+        <button onClick={getData}>getData</button>
+        <br/>
+        <pre>
+          {JSON.stringify(posts, null, 2)}
+        </pre>
     </div>
   )
 }
